a953ef76bb273570648178235c2e8bf1b39aed3c
Axel <axelghio@live.com>
2020-08-27 23:43:34 -0300

    subo tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicio1';
+
+  public EdadUno:string = '';
+  public EdadDos:string = '';
+  public ResultadoSuma:string = '';
+  public ResultadoPromedio:string = '';
+  
+  
+  constructor() { }
+
+  ngOnInit()
+  {
+
+  }
+
+  CalcularPromedio()
+  {
+    this.ResultadoSuma = (parseFloat(this.EdadUno) + parseFloat(this.EdadDos)).toString();
+    this.ResultadoPromedio = (parseFloat(this.ResultadoSuma) / 2).toString();
+  }
+
+
+
+  BorrarCuadros()
+  {
+    this.EdadUno = '';
+    this.EdadDos = '';
+    this.ResultadoPromedio = '';
+    this.ResultadoSuma = '';
+  }
 }
