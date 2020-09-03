4a7ee669e1275799e1790520fc677b93aa1b570b
Axel <axelghio@live.com>
2020-09-03 18:56:26 -0300

    Subo 2da Parte!

---
+++ src/app/Usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario
+{
+    public nombre : string;
+    public clave : string;
+
+    constructor() { }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
     
   ],
   imports: [
